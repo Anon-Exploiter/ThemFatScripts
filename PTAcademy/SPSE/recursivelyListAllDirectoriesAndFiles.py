6f88a12a7bc26eafb995aabdc208e9112883e687
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2019-12-21 11:57:27 +0500

    Create recursivelyListAllDirectoriesAndFiles.py
    This script will go ahead and list all the directories and files in `/etc/` and store the output in a JSON block and then will go ahead and print that in the console

---
+++ PTAcademy/SPSE/recursivelyListAllDirectoriesAndFiles.py
@@ -0,0 +1,21 @@
+"""
+This script will go ahead and list all the 
+directories and files in `/etc/` and store 
+the output in a json block and then will g-
+o ahead and print that in the console
+"""
+
+import json
+import os
+
+_files   = {}
+
+for presentDir, subdirs, files in os.walk('/home'):
+    for file in files:
+        if presentDir not in _files:
+            _files[presentDir] = [str( os.path.join(presentDir, file) )]
+
+        else:
+            _files[presentDir].append( str( os.path.join(presentDir, file) ) )
+
+print( json.dumps(_files, indent=4 ) )
