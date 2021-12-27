ebcc253337c4b6b26f6fc5cc470b083937b14a72
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2021-12-27 18:19:52 +0500

    Updated script for pfp change

--- Slack/changeProfilePicture.py
+++ Slack/changeProfilePicture.py
@@ -1,46 +1,70 @@
-import requests 
-import base64
+import requests
 import os
+import json
 
-with open('token.inc', 'r') as f: SLACK_TOKEN = base64.b64decode(f.read().strip()).decode().strip()
+SLACK_TOKEN = "xoxc-"
+HEADERS = {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Linux"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "cookie": "d=;",
+}
+
 
 def listDirectory(path):
-	images 		= os.listdir(path)
-	_images 	= []
+    images = os.listdir(path)
+    _images = []
 
-	for imageNames in images:
-		extension 	= imageNames.split(".")[::-1][0]
+    for imageNames in images:
+        extension = imageNames.split(".")[::-1][0]
 
-		if (extension == "png" or extension == "jpg" or extension == "gif" or extension == "svg" or extension == "jpeg"):
-			_images.append(imageNames)
+        if (
+            extension == "png"
+            or extension == "jpg"
+            or extension == "gif"
+            or extension == "svg"
+            or extension == "jpeg"
+        ):
+            _images.append(path + imageNames)
 
-	return(_images)
+    return _images
 
-def changeProfilePicture(pictures, slackToken):
-	for picture in pictures:
-		request 	= requests.post(
-			f"https://slack.com/api/users.setPhoto?token={SLACK_TOKEN}&pretty=1",
-			headers = {
-				"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36",
-				"Accept": "*/*",
-				"Origin": "https://api.slack.com",
-				"Sec-Fetch-Site": "same-site",
-				"Sec-Fetch-Mode": "cors",
-				"Accept-Encoding": "gzip, deflate",
-				"Accept-Language": "en-US,en;q=0.9,la;q=0.8",
-				"Connection": "close",
-			},
-			files = {
-				'image': open(picture, 'rb')
-			},
-		)
 
-		print(request.text)
+def changeProfilePicture(pictures):
+    for picture in pictures:
+        request = requests.post(
+            f"https://slack.com/api/users.preparePhoto",
+            headers=HEADERS,
+            files={"image": open(picture, "rb")},
+            data={"token": SLACK_TOKEN},
+        )
+
+        response = json.loads(request.text)
+        print(response)
+
+        if response["ok"] == True:
+            image_id = response["id"]
+
+            set_pfp = requests.post(
+                f"https://slack.com/api/users.setPhoto",
+                headers=HEADERS,
+                data={"id": image_id, "token": SLACK_TOKEN},
+            )
+
+            print(set_pfp.text)
+
 
 def main():
-	currentDirectory 	= os.getcwd()
-	filesInDirectory 	= listDirectory( currentDirectory )
-	changeProfilePicture(filesInDirectory, SLACK_TOKEN)
+    currentDirectory = "/home/user/scripts/pepe-images/"
+    filesInDirectory = listDirectory(currentDirectory)
 
-if __name__ == '__main__':
+    while True:
+        changeProfilePicture(filesInDirectory)
+
+
+if __name__ == "__main__":
     main()
